4c6e85d886a24de8c4da61b670d36d4c6a4b3bd0
Filipe2JD <127852282+FilipeCGEtec@users.noreply.github.com>
2023-08-29 10:37:40 -0300

    primeira classe criada

---
+++ js/personagem.js
@@ -0,0 +1,20 @@
+class Personagem {
+  constructor(posicao, velocidade, posicaoY, numeroDeFlores){
+    this.posicao = posicao;
+    this.velociadade = vel;
+    this.posicaoY = posY;
+    this.numeroDeFlores = numFlores;
+  }
+  morrer(){
+    
+  }
+  Andar(){
+
+  }
+  Correr(){
+    
+  }
+  ColetarFlor(){
+    
+  }
+}
